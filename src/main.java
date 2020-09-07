3cee84f45d75c769e6b18504933bcaeb187266f6
ashtonschmidt <ashtyjb10>
2020-09-07 14:49:01 -0600

    Try to write some of this code.

---
+++ src/main.java
@@ -0,0 +1,13 @@
+public class main
+{
+    public static void main (String [] args)
+    {
+        //this is an example of what it will look like when you print something and run it.
+        System.out.println("Good Luck!");
+        //code problem 1 here
+
+        //code problem 2 here
+
+        //code problem 3 here
+    }
+}
